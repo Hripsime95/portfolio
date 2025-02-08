6f5e7c6eaab8d0f549e4737d88b1d7d59d09be01
hripsime95 <h.vardoeva.95@gmail.com>
2025-02-09 03:14:39 +0400

    styled header

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -1,9 +1,10 @@
 // import React from "react";
 import styled from "styled-components";
 import { Logo } from "../../components/logo/logo";
-import { Menu } from "../../components/menu/Menu";
 import { Container } from "../../components/Container";
 import { FlexWrapper } from "../../components/FlexWrapper";
+import { Theme } from "../../styles/Theme";
+import { HeaderMenu } from "./headerMenu/HeaderMenu";
 
 const items = ["Home", "Skills", "Works", "Testimony", "Contact"];
 
@@ -13,7 +14,7 @@ export const Header = () => {
             <Container>
                 <FlexWrapper justify="space-between" align="center">
                     <Logo/>
-                    <Menu MenuItems={items}/>
+                    <HeaderMenu MenuItems={items}/>
                 </FlexWrapper>
                 
             </Container>
@@ -22,5 +23,10 @@ export const Header = () => {
 }
 
 const StyledHeader = styled.header`
-    background-color: beige;
+    background-color: ${Theme.colors.primaryBg};
+    padding: 20px 0;
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
 `
